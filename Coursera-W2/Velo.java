07bdc9e8dc032964659ffb82c976cf64a56586a9
Arthur Remy <arthur.remy.1712@free.fr>
2024-09-19 11:57:38 +0200

    exo 1 W2 coursera

--- Coursera-W2/Velo.java
+++ Coursera-W2/Velo.java
@@ -13,13 +13,40 @@ public class Velo {
         /*******************************************
          * Completez le programme a partir d'ici.
          *******************************************/
+        
+        if ( debut < 0 || fin < 0 || debut > 24 || fin > 24){
+            System.out.println("Les heures doivent Ãªtre comprises entre 0 et 24 !");
+        } else {
+            if ( debut == fin){
+             System.out.println("Bizarre, vous n'avez pas louÃ© votre vÃ©lo bien longtemps !");
+            } else {
+                if ( fin < debut){
+                    System.out.println("Bizarre, le dÃ©but de la location est aprÃ¨s la fin ...");
+                } else {
 
-        System.out.println("Les heures doivent Ãªtre comprises entre 0 et 24 !");
-        System.out.println("Bizarre, vous n'avez pas louÃ© votre vÃ©lo bien longtemps !");
-        System.out.println("Bizarre, le dÃ©but de la location est aprÃ¨s la fin ...");
-        System.out.println("Vous avez louÃ© votre vÃ©lo pendant");
-        System.out.print("Le montant total Ã  payer est de ");
-        System.out.println(" franc(s).");
+                    int duree = fin - debut;
+                    int dureeTarif1 = 0;
+
+                    if ((7 - debut) > 0){
+                        dureeTarif1 += 7 - debut ;
+                    } if ((fin - 17) > 0){
+                        dureeTarif1 += fin - 17 ;
+                    };
+                    
+                    int dureeTarif2 = duree - dureeTarif1;
+
+                    System.out.println("Vous avez loué votre vélo pendant");
+                    if (dureeTarif1 > 0){
+                        System.out.println(dureeTarif1 + "heure(s) au tarif horaire de 1.0 franc(s)");
+                    } if (dureeTarif2 > 0){
+                        System.out.println(dureeTarif2 + "heure(s) au tarif horaire de 2.0 franc(s)");
+                    };
+                    
+                    double prix = dureeTarif1 + dureeTarif2 * 2 ;
+                    System.out.println("Le montant total à payer est de " + prix + " franc(s)");   
+                }
+            }
+        }
 
         /*******************************************
          * Ne rien modifier apres cette ligne.
