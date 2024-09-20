94be502024c5b523eefb5446467cfa033cb780a7
Arthur Remy <arthur.remy.1712@free.fr>
2024-09-20 14:21:25 +0200

    amélioration exercice intervalles

--- Serie-03/Intervalle.java
+++ Serie-03/Intervalle.java
@@ -7,34 +7,15 @@ public class Intervalle{
         System.out.println("Entrez un réel : ");
         double x = scanner.nextDouble();
         
-        if ( x == 2 || x == 1 || x == -10 || x == -2){
+        boolean isIntervalBound = x == 2 || x == 1 || x == -10 || x == -2 ;
+        boolean inFirstInterval =  -10 < x && x < -2;
+        boolean inSecondInterval = 0 < x && x < 1;
+        boolean inThirdInterval = 2 < x && x < 3;
+
+        if (isIntervalBound || inFirstInterval || inSecondInterval || inThirdInterval) {
             System.out.println("x appartient à I");
-        } else {   
-            if ( -10 < x){
-                if ( x < -2){
-                    System.out.println("x appartient à I");
-                } else {
-                    if ( 0 < x) {
-                            if ( x < 1) {
-                                System.out.println("x appartient à I");
-                            } else {
-                                if ( 2 < x) {
-                                    if ( x < 3) {
-                                        System.out.println("x appartient à I");
-                                    } else {
-                                        System.out.println("x n'appartient pas à I");
-                                    }
-                            } else {
-                                System.out.println("x n'appartient pas à I");
-                            }
-                        }
-                    } else {
-                        System.out.println("x n'appartient pas à I");
-                    }
-                }
-            } else {
-                System.out.println("x n'appartient pas à I");
-            }
+        } else {
+            System.out.println("x n'appartient pas à I");
         }
     }
 }
