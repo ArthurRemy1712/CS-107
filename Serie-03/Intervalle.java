6faaa3bd6d1da8689b45f67bb6684d5aa0d96770
Arthur Remy <arthur.remy.1712@free.fr>
2024-09-25 14:34:58 +0200

    intégration des bornes aux intervalles exercice intervalle série 03

--- Serie-03/Intervalle.java
+++ Serie-03/Intervalle.java
@@ -7,12 +7,11 @@ public class Intervalle{
         System.out.println("Entrez un réel : ");
         double x = scanner.nextDouble();
         
-        boolean isIntervalBound = x == 2 || x == 1 || x == -10 || x == -2 ;
-        boolean inFirstInterval =  -10 < x && x < -2;
-        boolean inSecondInterval = 0 < x && x < 1;
-        boolean inThirdInterval = 2 < x && x < 3;
+        boolean inFirstInterval =  -10 <= x && x <= -2;
+        boolean inSecondInterval = 0 < x && x <= 1;
+        boolean inThirdInterval = 2 <= x && x < 3;
 
-        if (isIntervalBound || inFirstInterval || inSecondInterval || inThirdInterval) {
+        if (inFirstInterval || inSecondInterval || inThirdInterval) {
             System.out.println("x appartient à I");
         } else {
             System.out.println("x n'appartient pas à I");
---
+++ Serie-05/Pascal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Pascal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Entrez la taille du triangle de Pascal : ");
+        int n = scanner.nextInt();
+        
+    
+        int pascal[][] = 
