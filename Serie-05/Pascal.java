ddcb7957324e094d4dbf065d93736fbe81b5fed9
Arthur Remy <arthur.remy.1712@free.fr>
2024-09-27 13:26:51 +0200

    Fin Pascal.java

--- Serie-05/Pascal.java
+++ Serie-05/Pascal.java
@@ -5,7 +5,29 @@ public class Pascal {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print(" Entrez la taille du triangle de Pascal : ");
-        int n = scanner.nextInt();
+        int n = scanner.nextInt() + 1;
         
-    
-        int pascal[][] = 
+        int N = n * (n + 1) / 2;
+
+        int[] pascal = new int[N];
+
+        for ( int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (i < 2) {
+                        pascal[(i * (i + 1) / 2) + j] = 1;
+                    } else {
+                    pascal[(i * (i + 1) / 2) + j] = pascal[ ((i - 1) * i / 2) + j] + pascal[ ((i - 1) * i / 2) + j - 1];
+                    }
+                }
+        }
+        for ( int i = 0; i < n; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < i; j++) {
+                System.out.print(pascal[(i * (i + 1) / 2) + j] + " " );
+            }
+        }
+    }
+}
+        
+
+
