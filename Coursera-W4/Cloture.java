c3dbbacd6cd7622184b9b969215f3e3821cc9451
Arthur Remy <arthur.remy.1712@free.fr>
2024-09-27 18:11:57 +0200

    debut exercice cloture

--- Coursera-W4/Cloture.java
+++ Coursera-W4/Cloture.java
@@ -52,28 +52,242 @@ class Cloture {
             }
         }
         
-        int[][] carteSec = carte;
-        for (int i = 0; i < carteSec.length; i++) {
-            for (int j = 0; j < carteSec[i].length; j++) {
-                if (carteSec[i][j] == 0) {
+
+        ArrayList<Integer> abscisses = new ArrayList<Integer>();
+        ArrayList<Integer> ordonnees = new ArrayList<Integer>();
+
+        int composante = 1;
+
+        for (int i = 0; i < carte.length; i++) {
+            for (int j = 0; j < carte[i].length; j++) {
+                if (carte[i][j] == 0) {
+                    composante ++;
+                    abscisses.add(j);
+                    ordonnees.add(i);
+                    while ( abscisses.size() > 0) {
+                        int ordonnee = ordonnees.get(0);
+                        int abscisse = abscisses.get(0);
+                        if (carte[ordonnee][abscisse] == 0) {
+                            carte[ordonnee][abscisse] = composante;
+                            if (ordonnee == 0 && abscisse == 0) {
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee + 1);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else if (ordonnee == 0 && abscisse == carte[i].length - 1) {
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee+1);
+                                }
+                                if (carte[ordonnee][abscisse -1] == 0) {
+                                    abscisses.add(abscisse - 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else if (ordonnee == carte.length - 1 && abscisse == 0) {
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee - 1);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else if (ordonnee == carte.length - 1 && abscisse == carte[i].length - 1) {
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee - 1);
+                                }
+                                if (carte[ordonnee][abscisse - 1] == 0) {
+                                    abscisses.add(abscisse - 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else if (ordonnee == carte.length - 1) {
+                                if (carte[ordonnee][abscisse - 1] == 0) {
+                                    abscisses.add(abscisse -1);
+                                    ordonnees.add(ordonnee);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee -1);
+                                }
+                            } else if (ordonnee == 0) {
+                                if (carte[ordonnee][abscisse - 1] == 0) {
+                                    abscisses.add(abscisse -1);
+                                    ordonnees.add(ordonnee);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee +1);
+                                }
+                            } else if (abscisse == carte[i].length - 1) {
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee - 1);
+                                }
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee + 1);
+                                }
+                                if (carte[ordonnee][abscisse - 1] == 0) {
+                                    abscisses.add(abscisse - 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else if (abscisse == 0) {
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee - 1);
+                                }
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee + 1);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            } else {
+                                if (carte[ordonnee - 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee - 1);
+                                }
+                                if (carte[ordonnee + 1][abscisse] == 0) {
+                                    abscisses.add(abscisse);
+                                    ordonnees.add(ordonnee + 1);
+                                }
+                                if (carte[ordonnee][abscisse - 1] == 0) {
+                                    abscisses.add(abscisse - 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                                if (carte[ordonnee][abscisse + 1] == 0) {
+                                    abscisses.add(abscisse + 1);
+                                    ordonnees.add(ordonnee);
+                                }
+                            }
+                        }
+                        abscisses.remove(0);
+                        ordonnees.remove(0);
+
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < carte.length; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < carte[i].length; j++) {
+                System.out.print(carte[i][j] + " ");
+            }
+        }
+
+        ArrayList<Integer> zoneIds = new ArrayList<Integer>();
+
+        for (int i = 0; i < carte.length; i++) {
+            if (zoneIds.isEmpty()) {
+                zoneIds.add(carte[i][0]);
+            }
+            for (int j = 0; j <= zoneIds.size(); j++) {
+                if (j == zoneIds.size()) {
+                    zoneIds.add(carte[i][0]);
+                }else if (carte[i][0] == zoneIds.get(j)) {
+                    break;
+                }
+            }
+            for (int j = 0; j <= zoneIds.size(); j++) {
+                if (j == zoneIds.size()) {
+                    zoneIds.add(carte[i][carte[i].length - 1]);
+                }else if (carte[i][carte[i].length - 1] == zoneIds.get(j)) {
+                    break;
+                }
+            }
+        }
+        for (int i = 0; i < carte[0].length; i++) {
+            for (int j = 0; j <= zoneIds.size(); j++) {
+                if (j == zoneIds.size()) {
+                    zoneIds.add(carte[0][i]);
+                }else if (carte[0][i] == zoneIds.get(j)) {
+                    break;
+                }
+            }
+            for (int j = 0; j <= zoneIds.size(); j++) {
+                if (j == zoneIds.size()) {
+                    zoneIds.add(carte[0][carte.length - 1]);
+                }else if (carte[0][carte.length - 1] == zoneIds.get(j)) {
+                    break;
+                }
+            }
+        }
+        System.out.println("test");
+        for (int i = 0; i < carte.length; i++) {
+            for (int j = 0; j < carte[i].length; j++) {
+                if (carte[i][j] != 1) {
                     boolean Left1 = false;
                     boolean Right1 = false;
                     for (int k = 0; k < j; k++) {
-                        if (carteSec[i][k] == 1) {
+                        if (carte[i][k] == 1) {
                             Left1 = true;
                             break;
                         }
                     }
                     for (int k = j+1; k < carte[i].length; k++) {
-                        if (carteSec[i][k] == 1) {
+                        if (carte[i][k] == 1) {
                             Right1 = true;
                             break;
                         }
                     }
                     if (Left1 && Right1) {
-                        carteSec[i][j] = 1;
+                        for (int k = 0; k < zoneIds.size(); k ++) {
+                            if( zoneIds.get(k) == carte[i][j]) {
+                                System.out.println("Votre carte du terrain n'a pas le bon format :");
+                                System.out.print("bord extérieur entrant trouvé en position [");
+                                System.out.print(i);
+                                System.out.print("][");
+                                System.out.print(j);
+                                System.out.println("]");
+                            }
+                        }
                     }
                 }
+            }
+        }
+
+        for (int i = 0; i < carte.length; i++) {
+            for (int j = 0; j < carte[i].length; j++) {
+                if (carte[i][j] != 1) {
+                    boolean Left1 = false;
+                    boolean Right1 = false;
+                    for (int k = 0; k < j; k++) {
+                        if (carte[i][k] == 1) {
+                            Left1 = true;
+                            break;
+                        }
+                    }
+                    for (int k = j+1; k < carte[i].length; k++) {
+                        if (carte[i][k] == 1) {
+                            Right1 = true;
+                            break;
+                        }
+                    }
+                    if (Left1 && Right1) {
+                        carte[i][j] = 1;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < carte.length; i++) {
+            System.out.print("\n");
+            for (int j = 0; j < carte[i].length; j++) {
+                System.out.print(carte[i][j] + " ");
             }
         }
 
@@ -83,30 +297,32 @@ class Cloture {
             for (int j = 0; j < carte[i].length; j++) {
                 if (carte[i][j] == 1) {
                     if (i == 0 || i == carte.length - 1) {
-                        if (carte[i][j+1] == 0) {
+                        nCloture += 1;
+                        if (carte[i][j+1] != 1) {
                             nCloture += 1;
                         }
-                        if (carte[i][j-1] == 0) {
+                        if (carte[i][j-1] != 1) {
                             nCloture += 1;
                         }
                     } else if (j == 0 || j == carte[i].length - 1) {
-                        if (carte[i-1][j] == 0) {
+                        nCloture += 1;
+                        if (carte[i-1][j] != 1) {
                             nCloture += 1;
                         }
-                        if (carte[i+1][j] == 0) {
+                        if (carte[i+1][j] != 1) {
                             nCloture += 1;
                         }
                     } else {
-                        if (carte[i-1][j] == 0) {
+                        if (carte[i-1][j] != 1) {
                             nCloture += 1;
                         }
-                        if (carte[i+1][j] == 0) {
+                        if (carte[i+1][j] != 1) {
                             nCloture += 1;
                         }
-                        if (carte[i][j+1] == 0) {
+                        if (carte[i][j+1] != 1) {
                             nCloture += 1;
                         }
-                        if (carte[i][j-1] == 0) {
+                        if (carte[i][j-1] != 1) {
                             nCloture += 1;
                         }
                     }
@@ -124,10 +340,7 @@ class Cloture {
 
 
 
-        System.out.println("Votre carte du terrain n'a pas le bon format :");
-        System.out.print("bord extÃ©rieur entrant trouvÃ© en position [");
-        System.out.print("][");
-        System.out.println("]");
+
 
       
         /*******************************************
